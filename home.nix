5fcb4ee85b1c7f80112f5d97af6781800986f431
Noah Bresler <noahbresler2002@gmail.com>
2025-05-26 00:51:59 -0700

    Prompt

--- home.nix
+++ home.nix
@@ -35,12 +35,14 @@
     initContent =
       let
         zshConfigEarlyInit = lib.mkOrder 500 ''
-          # # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-          # # Initialization code that may require console input (password prompts, [y/n]
-          # # confirmations, etc.) must go above this block; everything else may go below.
-          if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-            source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-          fi
+          # From FAQ: https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#how-do-i-initialize-direnv-when-using-instant-prompt
+          (( ''${+commands[direnv]} )) && emulate zsh -c "$(direnv export zsh)"
+
+           if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+             source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+           fi
+
+           (( ''${+commands[direnv]} )) && emulate zsh -c "$(direnv hook zsh)"
         '';
         zshConfig = lib.mkOrder 1000 ''
                     source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme  
