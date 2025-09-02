8ee9268dc3f82aa40573e42539a969def9963be2
Noah Bresler <noahbresler2002@gmail.com>
2025-09-01 23:48:24 -0700

    refactor

--- home.nix
+++ home.nix
@@ -1,64 +1,73 @@
 { pkgs, lib, ... }:
 {
-  programs.direnv.enable = true;
-  programs.direnv.nix-direnv.enable = true;
+  programs = {
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
 
-  home.packages = with pkgs; [
-    gh # GitHub CLI
+    zsh = {
+      enable = true;
 
-    just
-    bun
-    tree
-    nodejs_24
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
 
-    # p10k
-    zsh-powerlevel10k
-    meslo-lgs-nf
+      initContent =
+        let
+          zshConfigEarlyInit = lib.mkOrder 500 ''
+            # From FAQ: https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#how-do-i-initialize-direnv-when-using-instant-prompt
+            (( ''${+commands[direnv]} )) && emulate zsh -c "$(direnv export zsh)"
 
-    # Nix LSP/formatter
-    nil
-    nixfmt-rfc-style
+             if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+               source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+             fi
 
-    # TODO:
-    # comma?
-
-    uv
-  ];
-
-  home.sessionPath = [ "/Users/noah/.bun/bin" ];
-
-  programs.zsh = {
-    enable = true;
-
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    initContent =
-      let
-        zshConfigEarlyInit = lib.mkOrder 500 ''
-          # From FAQ: https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#how-do-i-initialize-direnv-when-using-instant-prompt
-          (( ''${+commands[direnv]} )) && emulate zsh -c "$(direnv export zsh)"
-
-           if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-             source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-           fi
-
-           (( ''${+commands[direnv]} )) && emulate zsh -c "$(direnv hook zsh)"
-        '';
-        zshConfig = lib.mkOrder 1000 ''
-                    source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme  
-          source ~/.p10k.zsh
-
-        '';
-      in
-      lib.mkMerge [
-        zshConfigEarlyInit
-        zshConfig
-      ];
+             (( ''${+commands[direnv]} )) && emulate zsh -c "$(direnv hook zsh)"
+          '';
+          zshConfig = lib.mkOrder 1000 ''
+                      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme  
+            source ~/.p10k.zsh
+          '';
+        in
+        lib.mkMerge [
+          zshConfigEarlyInit
+          zshConfig
+        ];
+    };
   };
 
-  home.file.".p10k.zsh".text = builtins.readFile ./zsh/.p10k.zsh;
+  home = {
+    packages = with pkgs; [
+      gh # GitHub CLI
 
-  home.stateVersion = "25.05";
+      just
+      bun
+      tree
+      nodejs_24
+
+      # p10k
+      zsh-powerlevel10k
+      meslo-lgs-nf
+
+      # Nix LSP/formatter
+      nil
+      nixfmt-rfc-style
+
+      # TODO:
+      # comma?
+
+      uv
+    ];
+
+    sessionPath = [ "/Users/noah/.bun/bin" ];
+
+    shellAliases = {
+      "cy" = "claude --dangerously-skip-permissions";
+    };
+
+    file.".p10k.zsh".text = builtins.readFile ./zsh/.p10k.zsh;
+
+    stateVersion = "25.05";
+  };
 }
